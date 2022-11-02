c9bd21989037e046eddd0cc752617417d70b9509
Aaron McKay <aaronmckaynz@gmail.com>
2022-11-02 23:02:24 +1100

    Closer to this working

--- readme.md
+++ readme.md
@@ -42,3 +42,5 @@ https://github.com/zaproxy/zaproxy/issues/3497
 Headless ZAP
 **docker run -p 8090:8090 -i owasp/zap2docker-stable zap.sh -daemon -port 8090 -host 0.0.0.0 -config api.key=$env:APIKEY -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true**
 
+* Read the OWASP API
+https://www.zaproxy.org/docs/api/#overview
--- src/tests/owaspZap.test.ts
+++ src/tests/owaspZap.test.ts
@@ -2,46 +2,74 @@ import { test, expect } from "@playwright/test";
 import fetch from "node-fetch";
 
 test.describe("Confirm OWASP API", () => {
-	interface RootObject {
+	interface ZAPAlerts {
 		alertsByRisk: AlertsByRisk[];
 	}
 
 	interface AlertsByRisk {
-		Informational: Informational[];
-		Low: Low[];
-		Medium: Medium[];
-		High: High[];
+		Informational?: Alert[];
+		Low?: Alert[];
+		Medium?: Alert[];
+		High?: Alert[];
 	}
 
-	interface Medium {
-		[key: string]: AlertInstance[];
-	}
-	interface Low {
-		[key: string]: AlertInstance[];
-	}
-	interface Informational {
-		[key: string]: AlertInstance[];
-	}
-	interface High {
-		[key: string]: AlertInstance[];
+	interface Alert {
+		[key: string]: AlertInformation[];
 	}
 
-	interface AlertInstance {
+	interface AlertInformation {
 		param: string;
-		confidence: string;
+		confidence: Confidence;
 		name: string;
-		risk: string;
+		risk: Risk;
 		id: string;
 		url: string;
+	}
+
+	enum Confidence {
+		High = "High",
+		Low = "Low",
+		Medium = "Medium"
+	}
+
+	enum Risk {
+		Informational = "Informational",
+		Low = "Low",
+		Medium = "Medium"
 	}
 
 	test(`Confirm OWASP API`, async () => {
 		const url = `http://localhost:8090/JSON/alert/view/alertsByRisk/?apikey=${process.env.APIKEY}&baseurl='https://todomvc.com'&recurse=`;
 		console.log(url);
 		const response = await fetch(url);
-		const data: RootObject = await response.json();
-		console.log(data.alertsByRisk[0]);
+		const data: ZAPAlerts = await response.json();
 
-		//console.log(data.alertsByRisk.find(e=>e.Informational='Informational'))
+		console.log(data.alertsByRisk[0].Medium);
+		
+		if (data.alertsByRisk[0].Medium != undefined) {
+			console.log(data.alertsByRisk[0].Medium[0]);
+		}
+
+		let alertCount: number = 0;
+		let invalidAlertTypes: string[] = ["Medium", "High"];
+
+		for (let index = 0; index < data.alertsByRisk.length; index++) {
+			let currentAlertType = Object.keys(data.alertsByRisk[index])[0];
+
+			if (invalidAlertTypes.includes(currentAlertType)) {
+				console.log(`Risk Type is ${currentAlertType}`);
+				alertCount++;
+			}
+		}
+
+		if (alertCount >= 0) {
+			if (data.alertsByRisk[0].High != undefined) {
+				console.log(data.alertsByRisk[0].High[0]);
+			}
+			if (data.alertsByRisk[0].Medium != undefined) {
+				console.log(data.alertsByRisk[0].Medium[0]);
+			}
+			test.expect(alertCount).toBe(0);
+		}
 	});
 });
